034131bac816a3d45e6e02886ea4d3a50b7188c2
wangke <1015005300@qq.com>
2021-03-27 17:49:07 +0800

    update springboot and apollo version

--- spring-dubbo-impl/src/main/java/com/pupu/search/learn/impl/config/RecommendConfig.java
+++ spring-dubbo-impl/src/main/java/com/pupu/search/learn/impl/config/RecommendConfig.java
@@ -3,13 +3,13 @@ package com.pupu.search.learn.impl.config;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties(prefix = "recommend")
 @EnableApolloConfig
 @Configuration
 @Data
+@RefreshScope
 public class RecommendConfig {
 
     @Value("${recommend.recNum:10}")
